08d2a0a502f4922d5ea5d24bd8660a955159f88a
VirgilIw <imanuelambar11@gmail.com>
2026-02-26 12:56:30 +0700

    add id to  struct data response and complete crud

--- main.go
+++ main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
 
 type DataResponse struct {
+	ID       int    `json:"id"`
 	Email    string `json:"email" form:"email" binding:"required,email"`
 	Password string `json:"password" form:"password" binding:"required"`
 }
@@ -21,6 +23,7 @@ type Response struct {
 // omitempty = field kosong, jangan tampilkan di JSON response.
 
 var users []DataResponse
+var lastID int
 
 func main() {
 	r := gin.Default()
@@ -64,7 +67,8 @@ func main() {
 				return
 			}
 		}
-
+		lastID++
+		data.ID = lastID
 		users = append(users, data)
 
 		ctx.JSON(http.StatusCreated, Response{
@@ -74,5 +78,94 @@ func main() {
 		})
 	})
 
+	r.PATCH("/users/:id", func(ctx *gin.Context) {
+		idParam := ctx.Param("id")
+
+		id, err := strconv.Atoi(idParam)
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, Response{
+				Success: false,
+				Message: "invalid id",
+				Error:   err.Error(),
+			})
+			return
+		}
+
+		var data DataResponse
+
+		if err := ctx.ShouldBindJSON(&data); err != nil {
+			ctx.JSON(http.StatusBadRequest, Response{
+				Success: false,
+				Message: "bad request",
+				Error:   err.Error(),
+			})
+			return
+		}
+
+		found := false
+
+		for i, u := range users {
+			if u.ID == id {
+				found = true
+
+				if data.Email != "" {
+					users[i].Email = data.Email
+				}
+
+				if data.Password != "" {
+					users[i].Password = data.Password
+				}
+
+				break
+			}
+		}
+
+		if !found {
+			ctx.JSON(http.StatusNotFound, Response{
+				Success: false,
+				Message: "user not found",
+				Error:   "id does not exist",
+			})
+			return
+		}
+
+		ctx.JSON(http.StatusOK, Response{
+			Success: true,
+			Message: "user updated",
+			Data:    users,
+		})
+	})
+
+	r.DELETE("/users/:id", func(ctx *gin.Context) {
+		idParam := ctx.Param("id")
+
+		var result []DataResponse
+		found := false
+
+		for _, v := range users {
+			if strconv.Itoa(v.ID) == idParam {
+				found = true
+				continue
+			}
+			result = append(result, v)
+		}
+
+		if !found {
+			ctx.JSON(http.StatusNotFound, Response{
+				Success: false,
+				Message: "user not found",
+				Error:   "id does not exist",
+			})
+			return
+		}
+
+		users = result
+
+		ctx.JSON(http.StatusOK, Response{
+			Success: true,
+			Message: "success delete user",
+			Data:    result,
+		})
+	})
 	r.Run("localhost:8888")
 }
