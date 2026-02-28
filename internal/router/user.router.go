352f2e213ece4f439169a78d4816cf3f79264147
Virgil I Ambar <imanuelambar11@gmail.com>
2026-02-28 12:01:29 +0700

    feat: add user router

---
+++ internal/router/user.router.go
@@ -0,0 +1,17 @@
+package router
+
+import (
+	"koda-b6-backend1/internal/controller"
+
+	"github.com/gin-gonic/gin"
+)
+
+// Router pakai *gin.Engine → untuk mendaftarkan endpoint
+func UserRoutes(r *gin.Engine) {
+
+	users := r.Group("/users")
+
+	users.POST("", controller.CreateUser)
+	users.GET("", controller.GetUsers)
+	users.GET("/:id", controller.GetUserByID)
+}
