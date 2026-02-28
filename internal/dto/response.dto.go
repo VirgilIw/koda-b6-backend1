560f1644b78db1c3129a1ffc72abfbd14b7661e3
Virgil I Ambar <imanuelambar11@gmail.com>
2026-02-28 22:04:32 +0700

    feat: add response product

--- internal/dto/response.dto.go
+++ internal/dto/response.dto.go
@@ -6,3 +6,10 @@ type Response struct {
 	Error   string         `json:"error,omitempty"`
 	Data    []UserResponse `json:"data,omitempty"`
 }
+
+type ResponseProduct struct {
+	Success bool              `json:"success"`
+	Message string            `json:"message"`
+	Error   string            `json:"error,omitempty"`
+	Data    []ProductResponse `json:"data,omitempty"`
+}
