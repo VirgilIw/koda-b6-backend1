c9cb615c7f123d3ccaa6464cd991a84cc2da104a
Virgil I Ambar <imanuelambar11@gmail.com>
2026-03-01 17:01:28 +0700

    feat: add omitempty in struct product response

--- internal/dto/products.dto.go
+++ internal/dto/products.dto.go
@@ -1,12 +1,12 @@
 package dto
 
 type ProductResponse struct {
-	Id          int     `json:"id"`
-	Name        string  `json:"name"`
-	Description string  `json:"description"`
-	Rating      float64 `json:"rating"`
-	Stock       int     `json:"stock"`
-	Images      string  `json:"images"`
+	Id          int     `json:"id,omitempty"`
+	Name        string  `json:"name,omitempty"`
+	Description string  `json:"description,omitempty"`
+	Rating      float64 `json:"rating,omitempty"`
+	Stock       int     `json:"stock,omitempty"`
+	Images      string  `json:"images,omitempty"`
 }
 
 type ProductRequest struct {
