6b3e1841ed4e305d3e69695580aca48e25adbd7d
Virgil I Ambar <imanuelambar11@gmail.com>
2026-03-01 17:00:50 +0700

    feat: add controller for edit product, delete product, and get product by id

--- internal/controller/products.controller.go
+++ internal/controller/products.controller.go
@@ -4,6 +4,7 @@ import (
 	"koda-b6-backend1/internal/dto"
 	"koda-b6-backend1/internal/service"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -54,5 +55,117 @@ func GetAllProduct(ctx *gin.Context) {
 		Success: true,
 		Message: "success get all data",
 		Data:    data,
+	})
+}
+
+func GetProductById(ctx *gin.Context) {
+	idParam := ctx.Param("id")
+
+	id, err := strconv.Atoi(idParam)
+
+	if err != nil || id <= 0 {
+		ctx.JSON(http.StatusBadRequest, dto.ResponseProduct{
+			Success: false,
+			Message: "bad request",
+			Error:   "invalid product id",
+			Data:    []dto.ProductResponse{},
+		})
+		return
+	}
+
+	data, err := service.GetProductById(id)
+	if err != nil {
+		ctx.JSON(http.StatusNotFound, dto.ResponseProduct{
+			Success: false,
+			Message: "bad request",
+			Error:   err.Error(),
+			Data:    []dto.ProductResponse{},
+		})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, dto.ResponseProduct{
+		Success: true,
+		Message: "success get product",
+		Data:    []dto.ProductResponse{data},
+	})
+}
+
+func EditProductById(ctx *gin.Context) {
+
+	idParam := ctx.Param("id")
+	id, err := strconv.Atoi(idParam)
+
+	if err != nil || id <= 0 {
+		ctx.JSON(http.StatusBadRequest, dto.ResponseProduct{
+			Success: false,
+			Message: "bad request",
+			Error:   "invalid product id",
+			Data:    []dto.ProductResponse{},
+		})
+		return
+	}
+
+	var req dto.ProductRequest
+
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, dto.ResponseProduct{
+			Success: false,
+			Message: "invalid request body",
+			Error:   err.Error(),
+			Data:    []dto.ProductResponse{},
+		})
+		return
+	}
+
+	err = service.EditProductById(id, req)
+	if err != nil {
+		ctx.JSON(http.StatusNotFound, dto.ResponseProduct{
+			Success: false,
+			Message: "product not found",
+			Error:   err.Error(),
+			Data:    []dto.ProductResponse{},
+		})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, dto.ResponseProduct{
+		Success: true,
+		Message: "success edit product",
+		Data:    []dto.ProductResponse{},
+	})
+}
+
+func DeleteProductById(ctx *gin.Context) {
+	idParam := ctx.Param("id")
+
+	id, err := strconv.Atoi(idParam)
+
+	if err != nil || id <= 0 {
+		ctx.JSON(http.StatusBadRequest, dto.ResponseProduct{
+			Success: false,
+			Message: "bad request",
+			Error:   "invalid product id",
+			Data:    []dto.ProductResponse{},
+		})
+		return
+	}
+
+	err = service.DeleteProductById(id)
+
+	if err != nil {
+		ctx.JSON(http.StatusNotFound, dto.ResponseProduct{
+			Success: false,
+			Message: "product not found",
+			Error:   err.Error(),
+			Data:    []dto.ProductResponse{},
+		})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, dto.ResponseProduct{
+		Success: true,
+		Message: "product deleted",
+		Data:    []dto.ProductResponse{},
 	})
 }
